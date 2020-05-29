d1bee9cc2ade125aea30a438b50b39e4ac81edcb
Akhil T J <itexpert2572000@gmail.com>
2020-05-29 23:42:20 +0530

    dialog box added

--- app/src/main/java/com/akhiltj/mywebview/MainActivity.java
+++ app/src/main/java/com/akhiltj/mywebview/MainActivity.java
@@ -1,9 +1,13 @@
 package com.akhiltj.mywebview;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.DialogInterface;
 import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.view.View;
 import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
@@ -12,6 +16,7 @@ import android.widget.ProgressBar;
 public class MainActivity extends AppCompatActivity {
     private WebView webView;
     private ProgressBar progressBar;
+    @SuppressLint("SetJavaScriptEnabled")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,31 +27,72 @@ public class MainActivity extends AppCompatActivity {
 
         progressBar.setMax(100);
 
-        webView.setWebViewClient(new WebViewClient() {
+//        webView.setWebViewClient(new WebViewClient() {
+//            @Override
+//            public void onPageFinished(WebView view, String url) {
+//                super.onPageFinished(view, url);
+//                progressBar.setVisibility(View.GONE);
+//                setTitle(view.getTitle());
+//            }
+//
+//            @Override
+//            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+//                super.onPageStarted(view, url, favicon);
+//                progressBar.setVisibility(View.VISIBLE);
+//                setTitle("Loading...");
+//
+//            }
+//        });
+
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl("https://www.facebook.com");
+
+        webView.setWebViewClient(new WebViewClient());
+
+        webView.setWebChromeClient(new WebChromeClient() {
             @Override
-            public void onPageFinished(WebView view, String url) {
-                super.onPageFinished(view, url);
-                progressBar.setVisibility(view.GONE);
-                setTitle(view.getTitle());
+            public void onProgressChanged(WebView view, int newProgress) {
+                super.onProgressChanged(view, newProgress);
+                progressBar.setProgress(newProgress);
             }
 
             @Override
-            public void onPageStarted(WebView view, String url, Bitmap favicon) {
-                super.onPageStarted(view, url, favicon);
-                progressBar.setVisibility(view.VISIBLE);
-//                setTitle("Loading...");
+            public void onReceivedTitle(WebView view, String title) {
+                super.onReceivedTitle(view, title);
+            }
 
+            @Override
+            public void onReceivedIcon(WebView view, Bitmap icon) {
+                super.onReceivedIcon(view, icon);
             }
         });
-        webView.getSettings().setJavaScriptEnabled(true);
-        webView.loadUrl("https://itexpert2572000.github.io");
+
     }
 
     @Override
     public void onBackPressed() {
         if (webView.canGoBack()) {
             webView.goBack();
-        } else
-            super.onBackPressed();
+        }
+        else {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setMessage("Are you sure you want to exit?");
+            builder.setCancelable(false);
+            builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    MainActivity.super.onBackPressed();
+                }
+            });
+
+            builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    dialog.cancel();
+                }
+            });
+            AlertDialog alertDialog = builder.create();
+            alertDialog.show();
+        }
     }
 }
